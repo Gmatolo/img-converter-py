890db7806cff62408d45ba3d4a52a5da1efad64a
gmatolo <geraldmuli7@gmail.com>
2022-07-11 00:08:46 +0300

    iterates through image files in current working directory

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ import glob
 
 #identify, return and print pathnames ending wuth .png
 print(glob.glob("*.png"))
+
+#iterates through image files in current working directory
+for file in glob.glob("*.png"):
